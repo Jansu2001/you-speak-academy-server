11ca99e2c77ebddbde109730c520634f6b2896f6
jansu2001 <sadnansujan23@gmail.com>
2023-06-08 15:03:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,194 @@
+const express = require('express');
+const app =express()
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const port =process.env.PORT || 5000;
+
+
+// MiddleWare
+app.use(cors())
+app.use(express.json())
+
+// JWT Middle Ware
+// const verifyJWT= (req,res,next)=>{
+//     const authorization=req.headers.authorization;
+//     if(!authorization){
+//       return res.status(401).send({error: true, message:'unauthorization access'})
+//     }
+//     // 
+//     const token=authorization.split(' ')[1]
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+//       if(error){
+//         return res.status(403).send({error: true, message:'Forbidden access'})
+//       }
+//       req.decoded=decoded
+//       next()
+//     })
+//   }
+
+
+
+
+// Server Home Routes
+app.get('/', (req,res)=>{
+    res.send('Final Project is comming Soon')
+})
+
+
+
+
+const uri = `mongodb+srv://${process.env.ACADEMYDB_USER}:${process.env.ACADEMYDB_PASS}@cluster0.ceweuof.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usersCollection=client.db('speakAcademyDB').collection('allUsers')
+    const classesCollection=client.db('speakAcademyDB').collection('classes')
+
+
+
+
+    
+//     const verifyAdmin=async (req,res,next)=>{
+//         const email=req.decoded.email;
+//         const query={email:email}
+//         const user= await usersCollection.findOne(query)
+//         if(user?.role!=='admin'){
+//           return res.status(403).send({error: true, message:'Forbidden access'})
+//       }
+//      next()
+//   }
+  
+
+
+    // Get users
+    app.get('/users', async (req,res)=>{
+        const result= await usersCollection.find().toArray()
+        res.send(result)
+      })
+
+
+
+
+    //   JSW TOKEN FOR SECURUTY
+    // app.post('/jwt', (req,res)=>{
+    //     const user=req.body;
+    //     const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+    //     res.send({token})
+    //   })
+
+
+    app.post('/users', async (req, res) => {
+        const user=req.body
+        const query={email:user.email}
+        const existingUser=await usersCollection.findOne(query)
+        if(existingUser){
+          return res.send({message: 'user exists already'})
+        }
+        const result = await usersCollection.insertOne(user)
+        res.send(result)
+      })
+
+
+    //   Classess 
+    // Get Classes From MongoDB
+    app.get('/addclass', async (req,res)=>{
+        const result= await classesCollection.find().toArray()
+        res.send(result)
+      })
+
+    // POST Classes From Client Side
+    app.post('/addclass',async (req,res)=>{
+        const classes=req.body;
+        const result=await classesCollection.insertOne(classes)
+        res.send(result)
+      })
+
+
+
+
+
+
+
+    //   Make Uses Admin
+
+
+    // app.get('/users/admin/:email', async (req,res)=>{
+    //     const email=req.params.email
+    //     if(req.decoded.email!==email){
+    //       res.send({admin:false})
+    //     }
+    //     const query={email:email}
+    //     const user=await usersCollection.findOne(query)
+    //     const result ={admin: user?.role==='admin'}
+    //     res.send(result)
+  
+    //   })
+
+
+
+      app.patch('/users/admin/:id', async (req,res)=>{
+        const id=req.params.id;
+        const filter={_id: new ObjectId(id)} 
+        const updateDoc = {
+            $set: {
+              role: 'admin'
+            },
+          };
+          const result = await usersCollection.updateOne(filter,updateDoc)
+          res.send(result)
+      })
+    //   Make User Instructor
+      app.patch('/users/instructor/:id', async (req,res)=>{
+        const id=req.params.id;
+        const filter={_id: new ObjectId(id)} 
+        const updateDoc = {
+            $set: {
+              role: 'instructor'
+            },
+          };
+          const result = await usersCollection.updateOne(filter,updateDoc)
+          res.send(result)
+      })
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Final Project is comming Soon${port}`);
+})
